611a6ddbc08b74e61828f7cd2f5e2bdec27830ea
Owling797 <62678416+Owling797@users.noreply.github.com>
2021-11-23 14:46:33 +0300

    Update cardsman.h

--- cardsman.h
+++ cardsman.h
@@ -2,10 +2,12 @@
 #define CARDSMAN
 #include <iostream>
 #include <string>
+#include <math.h>
 
 class GamersDeck;
 class GameRules;
 class Game;
+class GeneralClass;
 
 class Card // класс карты
 {
@@ -30,13 +32,13 @@ public:
     void setSuit(char s) { suit = s; }
     Card *getPrev() { return prev; }
     Card *getNext() { return next; }
-    friend char translate(int);  // перевод номинала в текстовую нотацию (J, Q, K, A)
-    friend void printSuit(char); // вывод значка и цвета вместо буквы
+    friend char translate(int, char); // перевод номинала в текстовую нотацию (J, Q, K, A)
+    friend void printSuit(char);      // вывод значка и цвета вместо буквы
     friend GamersDeck;
     friend Game;
 };
 
-class Deck //класс базовой колоды
+/*class Deck //класс базовой колоды
 {
 protected:
     int number;
@@ -44,21 +46,24 @@ protected:
 public:
     Deck() { number = 0; };
     int getNumber() { return number; }
-};
+};*/
 
-class MainDeck : public Deck //
+class MainDeck //: public Deck //
 {
 protected:
     int **deckTable;
+    int jokers;
+    int number;
 
 public:
-    MainDeck() : Deck(){};
+    MainDeck() { number = 0; };
     void createTable(int);      // создание таблицы единиц по количеству карт в основной колоде
     void changeTable(int, int); // обнуление ячейки таблицы
+
     friend GameRules;
 };
 
-class GamersDeck : public Deck //колода игрока
+class GamersDeck // : public Deck //колода игрока
 {
 protected:
     Card *head;
@@ -66,7 +71,7 @@ protected:
     int cardsNumber = 0; // количество кард в колоде
 
 public:
-    GamersDeck() : Deck(){};
+    GamersDeck(){};
     Card *getHead() { return head; };
     Card *getTail() { return tail; };
     void setHead(Card *p) { head = p; };
@@ -100,6 +105,7 @@ public:
     void pass();                                       // пропустить ход и забрать карты в игре себе
     void setNext(Player *A) { next = A; }
     friend GameRules;
+    // friend UserInterface;
 };
 
 class List // связный однонаправленный список игроков
@@ -121,12 +127,21 @@ protected:
     int deckType;
     int gamers;
     MainDeck main_Deck;
+    int givemore; // 0 - не подкидн; 1 - подкидн
+    int pass;     // 0 - не переводн; 1 - переводн
+    int jokers;   // 0 - без; 1 - с ними
+    int addView;  // 1 - следующий, 2 - все
+    //int specTrump = 0;
 
 public:
-    GameRules(int type, int n)
+    GameRules(int type = 36, int n = 2, int gm = 0, int ps = 0, int jkrs = 0, int view = 0)
     {
         deckType = type;
         gamers = n;
+        givemore = gm;
+        pass = ps;
+        jokers = jkrs;
+        addView = view;
         main_Deck = MainDeck();
     };
 
@@ -134,20 +149,42 @@ public:
     char trumpSuit;     //козырная масть
     int winnersID = -1; // порядковый номер победителя
 
-    void setMainDeck() { main_Deck.createTable(deckType); } // создать основную колоду (банк) для заданного количества карт
-    Player *setPlayers();                                   // задать игроков и раздать им карты
-    int getType() { return deckType; }                      // получить количество карт в исходной колоде
-    void setTrumpSuit();                                    // установить случайную козырную масть
-    void giveMore(Player *);                                // додать игроку до 6 карт
-    void decreasePlayers() { gamers--; }                    // уменьшить на 1 текущее количество игроков в игре
-    int getPlayers() { return gamers; }                     // получить текущее количество игроков в игре
-    void removePlayer(Player *);                            // удалить из списка игрока
+    void setMainDeck()
+    {
+        main_Deck.jokers = jokers;
+        main_Deck.createTable(deckType);
+    }                     // создать основную колоду (банк) для заданного количества карт
+    Player *setPlayers(); // задать игроков и раздать им карты
+    void setGm(int n) { givemore = n; }
+    void setPass(int n) { pass = n; }
+    void setJokers(int n) { jokers = n; }
+    void setView(int n) { addView = n; }
+    int getType() { return deckType; }   // получить количество карт в исходной колоде
+    void setTrumpSuit();                 // установить случайную козырную масть
+    void giveMore(Player *);             // додать игроку до 6 карт
+    void decreasePlayers() { gamers--; } // уменьшить на 1 текущее количество игроков в игре
+    int getPlayers() { return gamers; }  // получить текущее количество игроков в игре
+    void removePlayer(Player *);         // удалить из списка игрока
+
+    friend GeneralClass;
+};
+
+class UserInterface
+{
+public:
+    void showOpponentNext(Player *);
+    void showOpponentAll(Player *);
+    void showInfoAttack(Player *, char);
+    void showInfoDefence(Player *, GamersDeck, char);
+    void results(int, int);
+    friend Game;
 };
 
 class Game : public GameRules // игра
 {
 public:
-    Game(int t, int n) : GameRules(t, n) {}
+    Game(int t = 36, int n = 2, int gm = 0, int ps = 0, int j = 0, int v = 0) : GameRules(t, n, gm, ps, j, v) {}
+
     GamersDeck attackCards;   // список карт, введенных отбивающимся игроком
     GamersDeck InGame;        // список карт, которых нужно побить (карты на отбой)
                               // int cycleMembers = 0;
@@ -155,7 +192,23 @@ public:
     void sort();              // отсортировать карты на отбой
     Player *Action(Player *); // основная функция игры
     int check_attack(Card *); // проверка, может ли карта побить какую-либо из карт на отбой
+    UserInterface UI;
+    friend GeneralClass;
     //int passToNext();
+};
+
+class GeneralClass : public Game
+{
+private: //флаги
+    int gm;
+    int ps;
+    int j;
+    int v;
+
+public:
+    GeneralClass(int, int, int, int);
+    void gamePlay(); // бывший main, тут все
+                     // void alonePlay();
 };
 
 #endif
